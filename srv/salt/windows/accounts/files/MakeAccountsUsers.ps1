d9b98b5471ed84ac52ad8a42b106593ae48c057d
Mike Swierczek <mike@swierczek.io>
2017-11-22 22:00:46 -0500

    Windows Users group management fixed.
    Now working properly.

--- srv/salt/windows/accounts/files/MakeAccountsUsers.ps1
+++ srv/salt/windows/accounts/files/MakeAccountsUsers.ps1
@@ -1,5 +1,15 @@
 
 $group = [ADSI]"WinNT://./Users,group"
 #echo "Group $group has members $group.members"
-#$group | Select-Object
-#$group.add("WinNT://$env:usertomanage,user")
+$members = $group.psbase.Invoke("Members")
+$found = $false
+$names = $members | ForEach-Object {
+   $_.GetType.Invoke().InvokeMember("Name", 'GetProperty', $null, $_, $null)
+}
+#echo $names
+if ($names -contains $env:usertomanage) {
+  echo "Found $env:usertomanage in group already."
+} else {
+  $group.add("WinNT://$env:usertomanage,user")
+  echo "Added $env:usertomanage to the Users group."
+}
